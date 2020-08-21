1916e4e5fb2e673630f728e6aed9747d8befa291
idriss <idriss.mortabit@gmail.com>
2020-08-21 17:38:41 +0100

    build folder

---
+++ client/build/precache-manifest.51c7c66888c77ff9649b15846812a1a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd500f073c5b3bb437784af57a845319",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9614bbdd2df442f59d56",
+    "url": "/static/css/main.cbc1b0d0.chunk.css"
+  },
+  {
+    "revision": "d714d67b29f0ca820f32",
+    "url": "/static/js/2.3275a3f9.chunk.js"
+  },
+  {
+    "revision": "9614bbdd2df442f59d56",
+    "url": "/static/js/main.816d0e5f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "303754d8a8952663c2ebfc5c9d38c976",
+    "url": "/static/media/logo-large.303754d8.png"
+  }
+]);
